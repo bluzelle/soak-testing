7952ff30215aa652ce25ed700ba7a168c7883105
John Lam <johnlamyc@gmail.com>
2018-12-18 13:54:42 -0800

    Update tests

--- test/performance-testing.js
+++ test/performance-testing.js
@@ -1,4 +1,4 @@
-const {BluzelleClient} = require('../bluzelle-js/lib/bluzelle-node');
+const {bluzelle} = require('../bluzelle-js/lib/bluzelle-node');
 const assert = require('assert');
 
 let api;
@@ -14,11 +14,15 @@ let api;
             '\nNumber of keys: ' + process.env.NUMBER_OF_KEYS
         );
 
-        api = new BluzelleClient(`ws://${process.env.ADDRESS}:${process.env.PORT}`, `${process.env.UUID}`, false);
+        api = bluzelle({
+            entry: `ws://${process.env.ADDRESS}:${process.env.PORT}`,
+            uuid: process.env.UUID,
+            private_pem: 'MHQCAQEEIFH0TCvEu585ygDovjHE9SxW5KztFhbm4iCVOC67h0tEoAcGBSuBBAAKoUQDQgAE9Icrml+X41VC6HTX21HulbJo+pV1mtWn4+evJAi8ZeeLEJp4xg++JHoDm8rQbGWfVM84eqnb/RVuIXqoz6F9Bg=='
+        });
 
-        await api.connect();
+        await api.createDB();
 
-        const options = {api, array: ARRAY_OF_NUM_OF_KEYS_SIZE, numberOfKeys, delay: 100};
+        const options = {api, arr: ARRAY_OF_NUM_OF_KEYS_SIZE, numberOfKeys, delayBetweenBatch: 100, batchSize: 10};
 
         const initialValue = 'hello';
         const updatedValue = 'newValue';
@@ -29,38 +33,16 @@ let api;
 
         await profiler(updateTest, {...options, value: updatedValue});
 
-        await profiler(removeTest, {...options});
+        await profiler(deleteTest, {...options});
 
-        api.disconnect();
         process.exit(0)
 
 })(parseInt(process.env.NUMBER_OF_KEYS)).catch((err) => {
     console.error(err);
-    api.disconnect();
     process.exit(1)
 });
 
-const removeTest = async (...args) => {
-
-    const {api} = args;
-
-    await batchOperation({...args, operation: 'remove'});
-
-    assert((await api.keys()).length === 0);
-};
-
-const readTest = async (...args) => {
-
-    const {numberOfKeys, value} = args;
-
-    const results = await batchOperation({...args, operation: 'read', delayBetweenBatch: 0, batchSize: 30});
-
-    assert(results.length === numberOfKeys);
-
-    assert(results.every((val) => val === value))
-};
-
-const createTest = async (...args) => {
+const createTest = async (args) => {
 
     const {api, numberOfKeys} = args;
 
@@ -71,25 +53,64 @@ const createTest = async (...args) => {
     assert(keys.length === numberOfKeys);
 };
 
-const updateTest = async (...args) => {
+const readTest = async (args) => {
 
-    const {value, array} = args;
+    const {numberOfKeys} = args;
+
+    const {value, ...argsWithoutValue} = args;
+
+    const results = await batchOperation({...argsWithoutValue, operation: 'read', batchSize: 10});
+
+    assert(results.length === numberOfKeys);
+
+    assert(results.every((val) => val === value))
+};
+
+const updateTest = async (args) => {
+
+    const {value, arr} = args;
 
     await batchOperation({...args, operation: 'update'});
 
-    assert((await api.read(`batch-key${array.length - 1}`)) === value);
+    assert((await api.read(`batch-key${arr.length - 1}`)) === value);
 };
 
-const batchOperation = async ({api, array, operation, value, delayBetweenBatch = 0, batchSize = 10} = {}) => {
+const deleteTest = async (args) => {
 
-    // debug undefined: api, array, operation, value, are all undefined here
-    console.log(array);
+    const {api} = args;
 
-    const batched = chunk(array, batchSize);
+    await batchOperation({...args, operation: 'delete'});
+
+    assert((await api.keys()).length === 0);
+};
+
+const profiler = async (fn, arguments) => {
+    const startTime = new Date();
+
+    await fn(arguments);
+
+    const endTime = new Date();
+    console.log(`Total time elapsed: ${endTime - startTime}ms`);
+};
+
+
+const batchOperation = async ({api, arr, operation, value, delayBetweenBatch, batchSize} = {}) => {
+
+    const batched = chunk(arr, batchSize);
 
     const delayedBatch = injectDelays(batched);
 
-    return await processMixedTypeArray({api, array: delayedBatch, delay: delayBetweenBatch, operation, value});
+    return await processMixedTypeArray({api, batch: delayedBatch, delayBetweenBatch, operation, value, batchSize});
+};
+
+const chunk = (arr, batchSize = 5) => {
+    const batched = [];
+
+    for (let i = 0; i < arr.length; i += batchSize) {
+        batched.push(arr.slice(i, i + batchSize))
+    }
+
+    return batched;
 };
 
 const injectDelays = (array) => array.reduce((acc, currentValue) => {
@@ -100,38 +121,23 @@ const injectDelays = (array) => array.reduce((acc, currentValue) => {
 
 const delay = ms => new Promise((resolve => setTimeout(resolve, ms)));
 
-const chunk = (array, batchSize = 5) => {
-    const batched = [];
 
-    for (let i = 0; i < array.length; i += batchSize) {
-        batched.push(array.slice(i, i + batchSize))
-    }
-
-    return batched;
-};
-
-const processMixedTypeArray = async ({api, array, delay, operation, value, batchSize}) => {
+const processMixedTypeArray = async ({api, batch, delayBetweenBatch, operation, value, batchSize}) => {
 
     console.log(`\nRunning ${operation}s, processing ${batchSize} keys per batch`);
-    if (delay > 0) {
-        console.log(`Delaying ${delay}ms between batch calls`)
+    if (delayBetweenBatch > 0) {
+        console.log(`Delaying ${delayBetweenBatch}ms between batch calls`)
     }
 
     let results = [];
     let returnValue;
 
-    for (element of array) {
-
-        // if (typeof(element) === 'object') {
-        //     console.log(`  Processing: ${element.length} keys`)
-        // } else {
-        //     // console.log(`  Awaiting delay`)
-        // }
+    for (element of batch) {
 
         let batchStartTime = new Date();
 
         if (typeof(element) === 'function') {
-            await element(delay)
+            await element(delayBetweenBatch)
         } else {
 
             try {
@@ -142,6 +148,7 @@ const processMixedTypeArray = async ({api, array, delay, operation, value, batch
                     if (returnValue !== undefined) {
                         results = [...results, ...returnValue]
                     }
+
                 } else {
                     await Promise.all(element.map((v) => api[operation]('batch-key' + v, value)))
                 }
@@ -156,13 +163,4 @@ const processMixedTypeArray = async ({api, array, delay, operation, value, batch
     }
 
     return results;
-};
-
-const profiler = async (fn, arguments) => {
-    const startTime = new Date();
-
-    await fn(arguments);
-
-    const endTime = new Date();
-    console.log(`Total time elapsed: ${endTime - startTime}ms`);
 };
--- test/smoke.test.js
+++ test/smoke.test.js
@@ -1,18 +1,43 @@
-const {BluzelleClient} = require('../bluzelle-js/lib/bluzelle-node');
+const {bluzelle} = require('../bluzelle-js/lib/bluzelle-node');
 const assert = require('assert');
 
 let api;
 
 describe('smoke tests', () => {
 
-    before('initialize client and connect', async () => {
-        api = new BluzelleClient(`ws://${process.env.ADDRESS}:${process.env.PORT}`, `${process.env.UUID}`, false);
-        await api.connect();
-    });
-
-    after('disconnect', () => api && api.disconnect());
-
     context('swarm', () => {
+
+        before('initialize client and createDB', async () => {
+
+            api = bluzelle({
+                entry: `ws://${process.env.ADDRESS}:${process.env.PORT}`,
+                uuid: process.env.UUID,
+                private_pem: 'MHQCAQEEIFH0TCvEu585ygDovjHE9SxW5KztFhbm4iCVOC67h0tEoAcGBSuBBAAKoUQDQgAE9Icrml+X41VC6HTX21HulbJo+pV1mtWn4+evJAi8ZeeLEJp4xg++JHoDm8rQbGWfVM84eqnb/RVuIXqoz6F9Bg=='
+            });
+
+            await api.createDB();
+        });
+
+        context('crud operations', () => {
+
+            it('should be able to create', async () => {
+                await api.create(`strKey`, 'abc');
+            });
+
+            it('should be able to read', async () => {
+                assert(await api.read(`strKey`) === 'abc');
+            });
+
+            it('should be able to update', async () => {
+                await api.update(`strKey`, 'abc def');
+                assert(await api.read(`strKey`) === 'abc def');
+            });
+
+            it('should be able to delete', async () => {
+                await api.delete(`strKey`);
+                assert(!await api.has(`strKey`))
+            });
+        });
 
         context('non crud operations', () => {
 
@@ -24,9 +49,9 @@ describe('smoke tests', () => {
             });
 
             after('clear database', async () => {
-                await api.remove('key0');
-                await api.remove('key1');
-                await api.remove('key2');
+                await api.delete('key0');
+                await api.delete('key1');
+                await api.delete('key2');
             });
 
             it('should be able to return size', async () => {
@@ -43,26 +68,33 @@ describe('smoke tests', () => {
                 assert.deepEqual(result.sort(), ['key2', 'key1', 'key0'].sort())
             });
         });
+    });
 
-        context('should be able to handle string fields', () => {
+    context('database management', () => {
 
-            it('should be able to create', async () => {
-                await api.create(`strKey`, 'abc');
-            });
+        let api2;
 
-            it('should be able to read', async () => {
-                assert(await api.read(`strKey`) === 'abc');
-            });
+        before('initialize client', async () => {
 
-            it('should be able to update', async () => {
-                await api.update(`strKey`, 'abc def');
-                assert(await api.read(`strKey`) === 'abc def');
-            });
-
-            it('should be able to remove', async () => {
-                await api.remove(`strKey`);
-                assert(!await api.has(`strKey`))
+            api2 = bluzelle({
+                entry: `ws://${process.env.ADDRESS}:${process.env.PORT}`,
+                uuid: process.env.UUID + 1,
+                private_pem: 'MHQCAQEEIFH0TCvEu585ygDovjHE9SxW5KztFhbm4iCVOC67h0tEoAcGBSuBBAAKoUQDQgAE9Icrml+X41VC6HTX21HulbJo+pV1mtWn4+evJAi8ZeeLEJp4xg++JHoDm8rQbGWfVM84eqnb/RVuIXqoz6F9Bg=='
             });
         });
-    });
+
+        it('should be able to createDB', async () => {
+            await api2.createDB();
+        });
+
+        it('should be able to hasDB', async () => {
+            assert(await api2.hasDB());
+        });
+
+        it('should be able to deleteDB', async () => {
+            await api2.deleteDB();
+            assert(!await api2.hasDB());
+        });
+
+    })
 });
